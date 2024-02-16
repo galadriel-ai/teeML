098f7badcc694cf7735801b20d0caec7c562bbe4
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-02-16 14:56:57 +0200

    Updates sh script

--- enclave/run_enclave.sh
+++ enclave/run_enclave.sh
@@ -2,6 +2,7 @@ ENCLAVE_ID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
 [ "$ENCLAVE_ID" != "null" ] && nitro-cli terminate-enclave --enclave-id ${ENCLAVE_ID}
 
 docker build ./ -t "galadriel"
+rm galadriel.eif
 nitro-cli build-enclave --docker-uri "galadriel:latest" --output-file "galadriel.eif"
 nitro-cli run-enclave --cpu-count 2 --memory 2048 --eif-path galadriel.eif
 nitro-cli describe-enclaves
