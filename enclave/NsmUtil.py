1586b88f4a536dc2c09adcf50e6eb844b9369e9d
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-02-22 10:21:57 +0200

    imports pysui

--- enclave/NsmUtil.py
+++ enclave/NsmUtil.py
@@ -11,6 +11,10 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 
+from pysui import SuiConfig
+from pysui import SyncClient
+from pysui.abstracts import PublicKey
+
 print("importing libnsm")
 import libnsm
 
