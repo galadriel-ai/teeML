759f22ac19323167816d9bb368a9d62004bc5b4a
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-02-22 11:07:57 +0200

    Adds traffic forwarder to connect to SUI.

--- enclave/run.sh
+++ enclave/run.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
+#!/bin/sh
+
+# Assign an IP address to local loopback
+ip addr add 127.0.0.1/32 dev lo
+
+ip link set dev lo up
+
+# TODO: for mainnet need to change
+# Add a hosts record, pointing target site calls to local loopback
+echo "127.0.0.1   https://fullnode.devnet.sui.io" >> /etc/hosts
+
+python3.10 /app/traffic_forwarder.py 127.0.0.1 443 3 8000 &
+
 # Start the server
 python3.10 /app/server.py
