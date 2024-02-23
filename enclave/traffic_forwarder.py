779c8ef6a80b0960d601fa7468a7fe208638f6bb
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-02-23 11:51:47 +0200

    tries to guess encoding

--- enclave/traffic_forwarder.py
+++ enclave/traffic_forwarder.py
@@ -2,19 +2,17 @@ import socket
 import sys
 import threading
 import time
+import guess_encoding
 
 
 def server(local_ip, local_port, remote_cid, remote_port):
     try:
-        dock_socket: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        dock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         dock_socket.bind((local_ip, local_port))
         dock_socket.listen(5)
 
         while True:
-            client_socket = dock_socket.accept()
-            print("client_socket:", client_socket)
-            client_socket = client_socket[0]
-            print("client_socket[0]:", client_socket)
+            client_socket = dock_socket.accept()[0]
 
             server_socket = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
             server_socket.connect((remote_cid, remote_port))
@@ -31,42 +29,28 @@ def server(local_ip, local_port, remote_cid, remote_port):
             outgoing_thread.start()
             incoming_thread.start()
     finally:
-        new_thread = threading.Thread(
-            target=server,
-            args=(local_ip, local_port, remote_cid,
-                  remote_port))
+        new_thread = threading.Thread(target=server,
+                                      args=(local_ip, local_port, remote_cid,
+                                            remote_port))
         new_thread.start()
 
     return
 
 
-def _get_destination_address(data: bytes):
-    print("\ndata:", data)
-    headers = data.decode().split('\r\n')
-    print("\nheaders:", headers)
-    # print("\nHeaders:", headers, "\n")
-    for header in headers:
-        if header.startswith('Host:'):
-            return header.split(': ')[1]
-    return None
-
-
-def forward(source: socket, destination: socket):
-    print("\ntraffic_forwarder, source:", source, ", destination:", destination)
+def forward(source, destination):
     string = ' '
     while string:
         try:
-            data = source.recv(1024)
-            destination_address = _get_destination_address(data)
-            print("destination_address:", destination_address)
-            if data:
-                destination.sendall(data)
+            string = source.recv(1024)
+            encoding = guess_encoding.execute(string)
+            print("encoding:", encoding)
+            if string:
+                destination.sendall(string)
             else:
                 source.shutdown(socket.SHUT_RD)
                 destination.shutdown(socket.SHUT_WR)
-        except:
-            # TODO Muted Forwarder Error - ConnectionResetError: [Errno 104] Connection reset by peer
-            print("")
+        except Exception as exc:
+            print("Exception in forward:", exc)
 
 
 def main(args):
