487a16d4a26a323793bc62e79611043a3fcb9eda
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-02-26 16:24:34 +0200

    Removes direct debug scripts

--- enclave/run.sh
+++ enclave/run.sh
@@ -16,8 +16,6 @@ python3.10 /app/traffic_forwarder.py 127.0.0.1 443 &
 
 # Debug
 sleep 30
-python3.10 /app/openai_call.py
-python3.10 /app/sui_debug.py
 python3.10 /app/check_proxies.py
 
 # Start the server
