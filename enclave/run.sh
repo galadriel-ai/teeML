68ca6571e7ff1f67f657195aad4c21caeea807dc
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-03-12 14:34:53 +0200

    adds encrypted envs

--- enclave/run.sh
+++ enclave/run.sh
@@ -12,6 +12,29 @@ echo "127.0.0.1   testnet.galadriel.com" >> /etc/hosts
 
 python3.10 /app/traffic_forwarder.py 127.0.0.1 443 &
 
+# Decrypt the .env file
+aws kms decrypt \
+    --ciphertext-blob fileb://.env.encrypted \
+    --key-id arn:aws:kms:eu-central-1:928498401497:key/b539ad32-e557-4479-aaa0-73344c3b9cf6 \
+    --output text \
+    --query Plaintext | base64 \
+    --decode > .env
+
+# Decrypt the service account key
+aws kms decrypt \
+    --ciphertext-blob fileb://sidekik-ai.json.encrypted \
+    --key-id arn:aws:kms:eu-central-1:928498401497:key/b539ad32-e557-4479-aaa0-73344c3b9cf6 \
+    --output text \
+    --query Plaintext | base64 \
+    --decode > sidekik-ai-5d0110872b7a.json
+
+#/app/kmstool_enclave_cli decrypt \
+#  --region "region" \
+#  --aws-access-key-id "access" \
+#  --aws-secret-access-key "secret" \
+#  --aws-session-token "token" \
+#  --ciphertext "ciphertext"
+
 python3.10 /app/key_manager.py
 
 # sleep so there is time to open enclave debug logs before the server potentially crashes
