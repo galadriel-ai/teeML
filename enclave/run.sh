6c038585293590be5b4476fed18854f6569fbdc0
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-03-12 12:11:48 +0200

    runs server in foreground

--- enclave/run.sh
+++ enclave/run.sh
@@ -12,12 +12,12 @@ echo "127.0.0.1   testnet.galadriel.com" >> /etc/hosts
 
 python3.10 /app/traffic_forwarder.py 127.0.0.1 443 &
 
-# Debug
+# sleep so there is time to open enclave debug logs before the server potentially crashes
 sleep 30
 python3.10 /app/check_proxies.py
 
 # Start the server
-python3.10 /app/server.py &
+python3.10 /app/server.py
 
 # Start oracle setup
 # python3.10 /app/oracle.py
