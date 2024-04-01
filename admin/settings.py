2a168b75812378528b75703431f1126e64052aa9
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-04-01 12:26:55 +0300

    Switches to devnet RPC

--- admin/settings.py
+++ admin/settings.py
@@ -11,6 +11,7 @@ SERPER_API_KEY = os.getenv("SERPER_API_KEY", "")
 
 CHAIN_ID = os.getenv("CHAIN_ID", "")
 WEB3_RPC_URL = os.getenv("WEB3_RPC_URL", "")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY", "")
 ORACLE_ADDRESS = os.getenv("ORACLE_ADDRESS", "")
 ORACLE_ABI_PATH = os.getenv("ORACLE_ABI_PATH", "")
 
--- enclave/run_proxies.sh
+++ enclave/run_proxies.sh
@@ -2,7 +2,7 @@ nohup vsock-proxy 8001 api.openai.com 443 \
   --config vsock/vsock_proxy_openai.yaml &> vsock_proxy_openai.log &
 nohup vsock-proxy 8002 google.serper.dev 443 \
   --config vsock/vsock_proxy_serper.yaml &> vsock_proxy_serper.log &
-nohup vsock-proxy 8003 testnet.galadriel.com 443 \
+nohup vsock-proxy 8003 devnet.galadriel.com 443 \
   --config vsock/vsock_proxy_galadriel.yaml &> vsock_proxy_galadriel.log &
 nohup vsock-proxy 8004 oaidalleapiprodscus.blob.core.windows.net 443 \
   --config vsock/vsock_proxy_windows.yaml &> vsock_proxy_windows.log &
